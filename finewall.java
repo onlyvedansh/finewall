290e1fec4fdc3307eac988ca3d28abc5ec169733
onlyvedansh <vedanshsinghkarki@gmail.com>
2025-07-08 00:05:13 +0530

    loadingwindow

--- finewall.java
+++ finewall.java
@@ -1,5 +1,9 @@
-import java.awt.Panel;
+
 import javafx.scene.control.TextArea;
+import javafx.scene.Parent;
+
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
 import javafx.concurrent.Task;
 import javafx.application.*;
 import javafx.geometry.*;
@@ -18,31 +22,61 @@ import java.io.File;
 
  public class finewall extends Application
 {
+    //linking to external css
    
-   public Button buttonM (String name , Pane parent)
+
+    //button method to add button in Panes :::button ( "button name" , Pane name)::
+   public void buttonM (String name , Pane parent)
    {
     Button button = new Button (name);
     parent.getChildren().add(button);
-    return button;
+    
 
    }
-    void loadingWindow()
+    void mainWindow()
     {
    
-        Stage loadingStage = new Stage();
-        loadingStage.setTitle("Finewall");
+        Stage mainStage = new Stage();
+        mainStage.setTitle("Finewall");
         Pane outerPane = new Pane();
         //outerPane.setLayoutX(50);outerPane.setLayoutY(50); 
         outerPane.setPrefSize(1000,500);
 
         HBox navBar = new HBox();
-        Button b=buttonM("home",navBar);
-        Button c =buttonM("About",navBar);
+        buttonM("home",navBar);
+        buttonM("About",navBar);
         outerPane.getChildren().add(navBar);
         Scene scene = new Scene (outerPane, 500,500);
-        loadingStage.show();
-        loadingStage.setScene(scene);
+        mainStage.show();
+        mainStage.setScene(scene);
 
+    }
+
+    void loadingWindow()
+    {     
+         Pane loadingOuterPane  = new Pane();
+         Scene scene = new Scene(loadingOuterPane,500,500);
+        Stage loadingStage = new Stage ();
+        scene.getStylesheets().add(getClass().getResource("finewall.css").toExternalForm());
+
+        loadingStage.setResizable(false);
+        loadingStage.show();
+        
+        Pane loadingInnerPane = new Pane ();
+        TextArea email = new TextArea();email.setPromptText("Enter email");
+
+        TextArea password = new TextArea();password.setPromptText("Enter email");
+        loadingOuterPane.getChildren().add(loadingInnerPane);
+        loadingInnerPane.getChildren().addAll(email,password);
+        buttonM("submit",loadingInnerPane);
+
+
+        PauseTransition delay = new PauseTransition(Duration.seconds(5));
+        delay.setOnFinished(e -> {
+           loadingStage.close();
+            mainWindow();
+         });
+          delay.play();
     }
      @Override
     public void start(Stage primaryStage) {
